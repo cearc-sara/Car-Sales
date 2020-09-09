99678ab229ffec2428864c37ea7c5a7bfc458b0d
Sara <scearc930@gmail.com>
2020-09-09 11:28:01 -0400

    working on adding total

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+export const ADD_FEATURE = "ADD_FEATURE"; //action type
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+// action suite
+export const addFeature = (feature) => {
+  //action creator
+  return {
+    //action
+    type: ADD_FEATURE,
+    payload: feature
+  };
+};
+
+export const removeFeature = (feature) => {
+    return{
+        type: REMOVE_FEATURE,
+        payload: feature
+    }
+}
